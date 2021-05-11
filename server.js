0614b5dd244eaba974630f585ff52bb42258ecf3
Raunak Raj <rounakraj122@gmail.com>
2021-05-11 19:20:00 +0545

    First Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const articleRouter = require('./routes/articles');
+const Article = require('./models/article');
+const config = require('./config.json')
+
+mongoose.connect(config.MongoDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex  : true,
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+
+    const articles = await Article.find().sort({ createdAt: 'desc'})
+    res.render('articles/index', { articles: articles })
+});
+
+app.use('/articles', articleRouter)
+app.listen(5000, () => {console.log('Running at http://localhost:5000/')});
